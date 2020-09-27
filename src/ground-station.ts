66835459cc80dd38a82c301705e6c083dd68ba2e
BuildTools <liam@anywhich.com>
2020-09-27 13:24:18 +1000

    ground station radio

--- src/ground-station.ts
+++ src/ground-station.ts
@@ -1,13 +1,13 @@
 import { Packet, PingPacket, ControlPacket } from './packet'
 import Radio from './radio'
 
-console.log('hello ground-station!')
-
 const radio = new Radio()
 
-let packet = new ControlPacket()
-
-console.log(packet)
-console.log(Packet.serialize(packet))
-console.log(Packet.deserialize(Packet.serialize(packet)))
+// console.log(packet)
+// console.log(Packet.serialize(packet))
+// console.log(Packet.deserialize(Packet.serialize(packet)))
 // radio.send(packet)
+
+setInterval(() => {
+  radio.send(new ControlPacket())
+}, 200)
